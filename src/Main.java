a99b9d46999c85653a98e6480a071602349ce5d6
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-07-17 10:59:12 +0500

    Yangi o'zgarish kirtdim.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Generation D27 group");
         System.out.println("Bugun 17-iyul 2024-yil");
         System.out.println("soat 10:38");
+        System.out.println("Bugun chorshanba");
     }
 }
