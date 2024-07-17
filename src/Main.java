bfa7c985ba856c71bc0e5281d5c4480928720fa9
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-07-17 10:39:52 +0500

    Yangi ma'lumot qo'shdim.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Generation D27 group");
+        System.out.println("Bugun 17-iyul 2024-yil");
+        System.out.println("soat 10:38");
     }
 }
