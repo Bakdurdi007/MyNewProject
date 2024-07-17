79d31959a1537f8e0fc4df6ec4f2687f9ed90512
bakdurdi-max <68939725+bakdurdi-max@users.noreply.github.com>
2024-07-17 10:49:00 +0500

    Man yangi class ochidim va unga HashMap hosil qildim. Foydalanivchi ma'lumotlarni saqlash uchun.

---
+++ src/DataBaseConnection.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class DataBaseConnection {
+    public static HashMap<Long, String> UserName = new HashMap();
+    public static HashMap<Long, String> Password = new HashMap();
+    public static HashMap<Long, String> Email = new HashMap();
+    public static HashMap<Long, String> Address = new HashMap();
+}
